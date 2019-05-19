a2be39e613af1ef4b57bffdb0479141f72af25e2
cybersecmoo <50770700+cybersecmoo@users.noreply.github.com>
2019-05-19 10:01:35 +0100

    Authenticate with Server

--- ssh_client.c
+++ ssh_client.c
@@ -2,11 +2,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void shutdownSession(ssh_session sesh) {
+    ssh_disconnect(sesh);
+    ssh_free(sesh);
+}
+
 int main(int argc, char **argv) {
     ssh_session sesh;
     int returnCode;
     int verbosity = SSH_LOG_PROTOCOL;
     int port = 22;
+    char *password = "foo";
 
     if(argc > 1) {
         port = atoi(argv[1]);
@@ -37,6 +43,16 @@ int main(int argc, char **argv) {
 
     printf("Authenticated the server\n");
 
-    ssh_disconnect(sesh);
-    ssh_free(sesh);
+    returnCode = ssh_userauth_password(sesh, NULL, password);
+
+    if(returnCode != SSH_AUTH_SUCCESS) {
+        fprintf(stderr, "*** ERROR: Failed to authenticate with server ***\n");
+        shutdownSession(sesh);
+
+        exit(-1);
+    }
+
+    printf("Authenticated with the Server\n");
+
+    shutdownSession(sesh);
 }
